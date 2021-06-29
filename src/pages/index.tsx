3d239eb9d01d9e6627ef920bfe4ad471504722b5
wedersonf <wedymf@hotmail.com>
2021-06-29 00:23:33 -0300

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,150 @@
+import Head from "next/head";
+import React from "react";
+
+import Header from "../components/Header";
+import { Footer } from "../components/Footer";
+
+import {
+  Container,
+  Wrapper,
+  Content,
+  ButtonContainer,
+  Section,
+  ContentHeading,
+  ContentFullText,
+  ContentImageLeft,
+  ContentImageRight,
+  FormContainer,
+  InputContent,
+  TextareaContent,
+  SubmitButton,
+} from "./styles";
+// eslint-disable-next-line
 export default function Home() {
   return (
-    <h1>Faz um bem</h1>
-  )
+    <>
+      <Head>
+        <title>Início | Faz um bem!</title>
+      </Head>
+
+      <Container>
+        <Header />
+
+        <Wrapper>
+          <Content>
+            <div>
+              <h1>
+                Famílias em situação de vulnerabilidade precisam da sua ajuda!
+              </h1>
+
+              <ButtonContainer>Quero ajudar!</ButtonContainer>
+            </div>
+
+            <img src="/imgs/together.svg" alt="Juntos" />
+          </Content>
+        </Wrapper>
+
+        <Section>
+          <h1>O que é o Faz um bem?</h1>
+          <ContentHeading>
+            <p>
+              “Faz um bem!” surge em um contexto de emergência de saúde pública
+              mundial com a pandemia de Coronavírus Disease 2019 (COVID-19) que
+              intensifica a necessidade de ações sociais por parte da sociedade
+              civil organizada e do Estado, especialmente através das políticas
+              públicas e sociais. Trata-se de um projeto de caráter
+              extensionista interdisciplinar, que visa desenvolver
+              colaborativamente e impulsionar uma plataforma virtual interativa
+              que centralize necessidades de instituições e organizações sociais
+              de Santa Maria. Assim, Faz um bem! propõe-se como uma plataforma
+              incentivadora e mediadora entre doadores e receptores de recursos.
+            </p>
+
+            <img src="/imgs/hugging.svg" alt="Abraçando" />
+          </ContentHeading>
+
+          <ContentFullText>
+            A plataforma se justifica pela necessidade constante das
+            instituições e entidades de assistência social divulgarem suas ações
+            e arrecadarem recursos com vistas à continuidade da prestação de
+            serviços e de benefícios socioassistenciais, necessários para a
+            subsistência de muitas famílias e melhoria das condições de vida na
+            perspectiva do acesso e usufruto de seus direitos sociais. Essas
+            necessidades são aumentadas no contexto da atual pandemia, que tem
+            tido consequência para as esferas social, econômica e política.
+          </ContentFullText>
+
+          <ContentFullText>
+            A plataforma conta com uma ampla equipe Coordenada por professores
+            de diversos Centros da Universidade Federal de Santa Maria. Essa
+            parceria permite a inclusão de estudantes de áreas de conhecimentos
+            distintas, possibilitando o enriquecimento da formação acadêmica dos
+            estudantes na perspectiva da interdisciplinaridade e do trabalho em
+            equipe.
+          </ContentFullText>
+
+          <ContentImageLeft>
+            <img src="/imgs/hugging.svg" alt="" />
+            <p>
+              A plataforma cadastra campanhas e instituições que possam fazer a
+              diferença na vida de famílias e comunidas. Você busca campanhas ou
+              instituições e vê como pode ajudar. As ajudas funcionam
+              diretamente com os promotores das campanhas, os quais abastecem a
+              plataforma com informações. É responsabilidade da plataforma
+              conectar doadores e campanhas, tornando mais fácil a visualização
+              e o acesso.
+            </p>
+          </ContentImageLeft>
+
+          <ContentImageRight>
+            <p>
+              As intituições são responsáveis pelo conteúdo disponibilizado,
+              assim como pela execução das doações. A plataforma não é
+              responsável pela captação de recursos, nem pela entrega às
+              instituições.
+            </p>
+            <img src="/imgs/hugging.svg" alt="" />
+          </ContentImageRight>
+
+          <ContentImageLeft>
+            <img src="/imgs/hugging.svg" alt="" />
+            <p>
+              As instituições são responsáveis pelo conteúdo disponibilizado,
+              assim como pela execução das doações. A plataforma não é
+              responsável pela captação de recursos, nem pela entrega às
+              instituições.
+            </p>
+          </ContentImageLeft>
+
+          <ContentImageRight>
+            <p>
+              Para ser um apoiador, ajude a divulgar essa plataforma, para que
+              chegue a um número enorme de pessoas que possam fazer a diferença.
+              Entre em contato pelo email: fazumbem@inf.ufsm.br e saiba mais.
+            </p>
+            <img src="/imgs/hugging.svg" alt="" />
+          </ContentImageRight>
+
+          <ContentImageLeft id="contact">
+            <img src="/imgs/hugging.svg" alt="" />
+
+            <FormContainer>
+              <InputContent placeholder="Nome*" />
+              <InputContent placeholder="Telefone*" />
+              <InputContent placeholder="E-mail*" />
+
+              <TextareaContent placeholder="Mensagem*" />
+
+              <div>
+                <span>(Todos os campos* são obrigatórios)</span>
+                <SubmitButton type="submit">Enviar</SubmitButton>
+              </div>
+            </FormContainer>
+          </ContentImageLeft>
+        </Section>
+
+        <Footer />
+      </Container>
+    </>
+  );
 }
---
+++ src/pages/styles.ts
@@ -0,0 +1,138 @@
+import styled from "styled-components";
+import Button from "../components/Button";
+
+import { Input } from "../components/Input";
+import { Textarea } from "../components/Textarea";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Wrapper = styled.div`
+  height: calc(100vh - 80px);
+  width: 100%;
+  background: linear-gradient(120deg, #034074, rgba(3, 64, 115, 0.6));
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Content = styled.main`
+  max-width: 1120px;
+  width: 100%;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  margin: auto;
+
+  div {
+    margin: auto 0;
+
+    h1 {
+      color: var(--white);
+      font-size: 4rem;
+      line-height: 4rem;
+      width: 440px;
+      font-weight: normal;
+    }
+  }
+`;
+
+export const ButtonContainer = styled(Button)`
+  background: var(--yellow);
+  margin-top: 8rem;
+  height: 6rem;
+  width: 40rem;
+`;
+
+export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  max-width: 1120px;
+  width: 100%;
+
+  margin: 3rem 0;
+
+  h1 {
+    font-size: 3rem;
+    color: var(--heading);
+  }
+`;
+
+export const ContentHeading = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  margin-top: 1rem;
+
+  p {
+    font-size: 1.5rem;
+    text-align: justify;
+  }
+
+  img {
+    margin: 0 auto;
+    width: 28rem;
+  }
+`;
+
+export const ContentFullText = styled.p`
+  font-size: 1.5rem;
+  margin-top: 3rem;
+  text-align: justify;
+`;
+
+export const ContentImageLeft = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+
+  margin-top: 3rem;
+
+  p {
+    font-size: 1.5rem;
+    text-align: justify;
+  }
+`;
+
+export const ContentImageRight = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+
+  margin-top: 3rem;
+
+  p {
+    font-size: 1.5rem;
+    text-align: justify;
+  }
+`;
+
+export const FormContainer = styled.form`
+  max-width: 350px;
+  margin: 3rem auto 0;
+
+  div {
+    display: grid;
+    grid-template-columns: 1.5fr 1fr;
+
+    span {
+      font-size: 1rem;
+      color: var(--description);
+    }
+  }
+`;
+
+export const InputContent = styled(Input)``;
+
+export const TextareaContent = styled(Textarea)`
+  margin: 1rem 0;
+`;
+
+export const SubmitButton = styled(Button)`
+  height: 4rem;
+  font-size: 1.5rem;
+`;
