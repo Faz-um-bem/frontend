ce1e8c9892e0d717b154f19524f68bb0aad23fb5
wedersonf <wedymf@hotmail.com>
2021-06-29 00:21:09 -0300

    footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Container } from "./styled";
+
+export function Footer() {
+  return (
+    <Container>
+      <h1>&copy; 2021 - Faz um bem!</h1>
+      <span>Feito com s2 por Universidade Federal de Santa Maria</span>
+    </Container>
+  );
+}
---
+++ src/components/Footer/styled.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+  background: linear-gradient(120deg, #034074, rgba(3, 64, 115, 0.6));
+  width: 100%;
+  height: 6rem;
+
+  display: flex;
+  flex-direction: column;
+
+  justify-content: center;
+  align-items: center;
+
+  color: var(--white);
+
+  h1 {
+    font-size: 1.5rem;
+    font-weight: normal;
+  }
+
+  span {
+    font-size: 1.5rem;
+  }
+`;
