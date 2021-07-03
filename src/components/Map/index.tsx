bfd372b9bfa2f3db1d7adf15b0c70b28133834f1
wedersonf <wedymf@hotmail.com>
2021-07-03 17:52:31 -0300

    config map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,25 +1,98 @@
 import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import Link from "next/link";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import Leaflet from "leaflet";
 
 import "leaflet/dist/leaflet.css";
 
 import { Container } from "./styles";
 
-const Map: React.FC = () => {
+const mapIcon = Leaflet.icon({
+  iconUrl: 'imgs/marker.svg',
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+type Location = {
+  latitude: number;
+  longitude: number;
+}
+
+type Place = {
+  id: string;
+  name: string;
+  slug: string;
+  location: Location;
+}
+
+type MapProps = {
+  initialLocation: Location;
+  places?: Place[];
+}
+
+
+export default function Map({ initialLocation, places = [] }: MapProps) {
   return (
     <Container>
       <MapContainer
-        center={[-29.6987317, -53.8780534]}
-        zoom={15}
-        style={{ width: "100%", height: "100%" }}
+        // center={[-29.6987317, -53.8780534]}
+        center={[initialLocation?.latitude || 0, initialLocation?.longitude || 0]}
+        zoom={13}
+        style={{ width: "100%", height: "100%", borderRadius: "inherit" }}
         scrollWheelZoom={false}
       >
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`}
         />
+
+        {places?.map(place => (
+          <Marker key={place.id} icon={mapIcon} position={[place.location.latitude, place.location.longitude]}>
+            <Popup
+              closeButton={false}
+              minWidth={240}
+              maxWidth={240}
+              className="map-popup"
+            >
+              {[place.name]}
+
+              <Link href={`/institution/${place.slug}`}>
+                >
+              </Link>
+            </Popup>
+          </Marker>
+        ))}
+
+        {/* <Marker icon={mapIcon} position={[-29.6987317, -53.8780534]}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            Teste
+
+            <Link href="/institution">
+              >
+            </Link>
+          </Popup>
+        </Marker>
+
+        <Marker icon={mapIcon} position={[-29.7063996, -53.818438]}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            Teste
+
+            <Link href="/institution">
+              >
+            </Link>
+          </Popup>
+        </Marker> */}
       </MapContainer>
     </Container>
   );
 };
-
-export default Map;
--- src/components/Map/styles.ts
+++ src/components/Map/styles.ts
@@ -1,8 +1,41 @@
 import styled from "styled-components";
-
+// eslint-disable-next-line
 export const Container = styled.div`
-  width: 500px;
-  height: 300px;
-`;
+  width: 100%;
+  height: 100%;
+  border-radius: 1rem;
+  box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.1);
 
-export const teste = styled.div``;
+  .map-popup .leaflet-popup-content-wrapper {
+    background: rgba(255, 255, 255, 0.8));
+    border-radius: 2rem;
+    box-shadow: none;
+  }
+
+  .map-popup .leaflet-popup-content {
+    color: var(--heading);
+    font-size: 2rem;
+    font-weight: bold;
+    margin: 1rem 1.5rem;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .map-popup .leaflet-popup-content a {
+    width: 40px;
+    height: 40px;
+    border-radius: 12px;
+    background: var(--blue);
+    box-shadow: 17.2868px 27.6589px 41.4884px rgba(23, 142, 166, 0.16);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .map-popup .leaflet-popup-tip-container {
+    display: none;
+  }
+`;
