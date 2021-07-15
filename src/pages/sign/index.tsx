a1495881a0e0650e896f6b8dfa4fea1f9741d91f
wedersonf <wedymf@hotmail.com>
2021-07-14 21:04:07 -0300

    Página de login

--- src/pages/sign/index.tsx
+++ src/pages/sign/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import React from 'react';
 import { HiDocumentSearch } from 'react-icons/hi';
 import { FaBuilding } from 'react-icons/fa';
+import { useRouter } from 'next/router';
 
 import Header from '../../components/Header';
 import { Footer } from '../../components/Footer';
@@ -10,6 +11,12 @@ import OptionItem from '../../components/OptionItem';
 import { Container, Content, Items } from './styles';
 
 export default function sign() {
+  const router = useRouter();
+
+  const handleCampaign = (type: string) => {
+    router.push(`/sign/${type}/signin`);
+  };
+
   return (
     <>
       <Head>
@@ -21,8 +28,16 @@ export default function sign() {
 
         <Content>
           <Items>
-            <OptionItem label="Sou uma instituição" icon={FaBuilding} />
-            <OptionItem label="Sou um(a) curador(a)" icon={HiDocumentSearch} />
+            <OptionItem
+              label="Sou uma instituição"
+              icon={FaBuilding}
+              onClick={() => handleCampaign('institution')}
+            />
+            <OptionItem
+              label="Sou um(a) curador(a)"
+              icon={HiDocumentSearch}
+              onClick={() => handleCampaign('curator')}
+            />
           </Items>
         </Content>
 
