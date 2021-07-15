660c9961a4edc662f2a38ca45cd8ef4954ea13b1
wedersonf <wedymf@hotmail.com>
2021-07-15 03:50:43 -0300

    Adicionado página dashboard

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,84 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { FaEdit } from 'react-icons/fa';
+
+import Header from '../../components/Header';
+import OptionItem from '../../components/OptionItem';
+import { Footer } from '../../components/Footer';
+
+import { Container, Content, OptionList } from './styles';
+
+export default function Dashboard() {
+  const router = useRouter();
+
+  function handleSubmit(url: string) {
+    router.push(url);
+  }
+
+  const institutionItems = [
+    <OptionItem
+      label="Editar Perfil"
+      icon={FaEdit}
+      onClick={() => handleSubmit('/dashboard/manage/profile')}
+      disabled
+    />,
+    <OptionItem
+      label="Gerênciar Campanhas"
+      icon={FaEdit}
+      onClick={() => handleSubmit('/dashboard/manage/campaigns')}
+    />,
+    <OptionItem
+      label="Gerênciar Galeria"
+      icon={FaEdit}
+      onClick={() => handleSubmit('/dashboard/manage/gallery')}
+    />,
+    <OptionItem
+      label="Histórico de Atualizações"
+      icon={FaEdit}
+      onClick={() => handleSubmit('/dashboard/history')}
+      disabled
+    />,
+  ];
+
+  // const curatorAdminItems = [
+  //   <OptionItem
+  //     label="Editar Perfil"
+  //     icon={FaEdit}
+  //     onClick={() => handleSubmit('/dashboard/manage/profile')}
+  //     disabled
+  //   />,
+  //   <OptionItem
+  //     label="Gerênciar Curadores"
+  //     icon={FaEdit}
+  //     onClick={() => handleSubmit('/dashboard/manage/curators')}
+  //   />,
+  //   <OptionItem
+  //     label="Auditar Instituições"
+  //     icon={FaEdit}
+  //     onClick={() => handleSubmit('/dashboard/manage/institutions')}
+  //   />,
+  //   <OptionItem
+  //     label="Auditar Campanhas"
+  //     icon={FaEdit}
+  //     onClick={() => handleSubmit('/dashboard/manage/campaigns')}
+  //   />,
+  // ];
+
+  return (
+    <>
+      <Head>
+        <title>Painel do usuário | Faz um bem!</title>
+      </Head>
+
+      <Container>
+        <Header />
+
+        <Content>
+          <OptionList>{institutionItems.map(item => item)}</OptionList>
+        </Content>
+
+        <Footer />
+      </Container>
+    </>
+  );
+}
---
+++ src/pages/dashboard/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  min-height: calc(100vh - 80px - 6rem);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2.5rem 0;
+`;
+
+export const OptionList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 2rem;
+`;
