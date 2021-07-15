6e7e33d607b558364f854ffa9e2324e9ede85ea4
wedersonf <wedymf@hotmail.com>
2021-07-15 03:51:08 -0300

    Adicionado página de gerenciar campanhas

---
+++ src/pages/dashboard/manage/campaigns/index.tsx
@@ -0,0 +1,55 @@
+import Head from 'next/head';
+import { CardListItem } from '../../../../components/CardListItem';
+
+import { Footer } from '../../../../components/Footer';
+import Header from '../../../../components/Header';
+
+import { Container, CampaignList, Content } from './styled';
+
+export default function ManageCampaign() {
+  return (
+    <>
+      <Head>
+        <title>Gerênciar Campanhas | Faz um bem!</title>
+      </Head>
+
+      <Container>
+        <Header />
+
+        <Content>
+          <h1>Gerênciar Campanhas</h1>
+
+          <CampaignList>
+            <CardListItem
+              title="teste1"
+              description="HFUSDAGHASDUIFHGUISDAHFGIUDASHSIAUDHFIUASHGIUSADHFIUGHSADIUFHGIUSADHFUGSADHIFHGSUAIDHFSIA"
+              status="active"
+            />
+            <CardListItem
+              title="teste2"
+              description="HFUSDAGHASDUIFHGUISDAHFGIUDASHSIAUDHFIUASHGIUSADHFIUGHSADIUFHGIUSADHFUGSADHIFHGSUAIDHFSIA"
+              status="draft"
+            />
+            <CardListItem
+              title="teste3"
+              description="HFUSDAGHASDUIFHGUISDAHFGIUDASHSIAUDHFIUASHGIUSADHFIUGHSADIUFHGIUSADHFUGSADHIFHGSUAIDHFSIA"
+              status="active"
+            />
+            <CardListItem
+              title="teste4"
+              description="HFUSDAGHASDUIFHGUISDAHFGIUDASHSIAUDHFIUASHGIUSADHFIUGHSADIUFHGIUSADHFUGSADHIFHGSUAIDHFSIA"
+              status="active"
+            />
+            <CardListItem
+              title="teste5"
+              description="HFUSDAGHASDUIFHGUISDAHFGIUDASHSIAUDHFIUASHGIUSADHFIUGHSADIUFHGIUSADHFUGSADHIFHGSUAIDHFSIA"
+              status="active"
+            />
+          </CampaignList>
+        </Content>
+
+        <Footer />
+      </Container>
+    </>
+  );
+}
