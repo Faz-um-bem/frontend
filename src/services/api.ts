0422045b77d47a1ddce587418f4e18215124e9c0
wedersonf <wedymf@hotmail.com>
2021-07-16 03:30:13 -0300

    Setando headers

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
+import { parseCookies } from 'nookies';
+
+const cookies = parseCookies();
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    Authorization: `Bearer ${cookies['fazumbem.token']}`,
+  },
 });
 
 export default api;
