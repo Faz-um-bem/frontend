583653ca1bc9db16566f6c4a873dc9b9f356363e
wedersonf <wedymf@hotmail.com>
2021-07-17 22:31:47 -0300

    Refatorando página inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import Head from 'next/head';
-import React from 'react';
+import Image from 'next/image';
+import React, { useCallback } from 'react';
+import { FiMail, FiPhone, FiUser } from 'react-icons/fi';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
@@ -10,17 +15,48 @@ import {
   Content,
   ButtonContainer,
   Section,
-  ContentHeading,
-  ContentFullText,
-  ContentImageLeft,
-  ContentImageRight,
+  MoreInfo,
+  MoreInfoContent,
   FormContainer,
   InputContent,
   TextareaContent,
   SubmitButton,
+  BackgroundContent,
+  ContactContainer,
 } from './styles';
 
+type SignInFormData = {
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+};
+
+const formSchema = yup.object().shape({
+  name: yup.string().required('Nome é obrigatório'),
+  email: yup.string().required('E-mail é obrigatório').email(),
+  phone: yup
+    .string()
+    .min(11, 'O número deve conter DDD + Número')
+    .required('Telefone obrigatório'),
+  message: yup.string().required('A mesangem é obrigatória'),
+});
+
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+  const { errors } = formState;
+
+  const handleSubmitForm: SubmitHandler<SignInFormData> = useCallback(
+    async (data, event) => {
+      await new Promise(resolve => setTimeout(resolve, 3000));
+      event.preventDefault();
+      console.log(data);
+    },
+    [],
+  );
+
   return (
     <>
       <Head>
@@ -31,39 +67,49 @@ export default function Home() {
         <Header />
 
         <Wrapper>
+          <BackgroundContent />
           <Content>
             <div>
-              <h1>
-                Famílias em situação de vulnerabilidade precisam da sua ajuda!
-              </h1>
+              <span>
+                FAMÍLIAS EM SITUAÇÃO DE <strong>VULNERABILIDADE</strong>{' '}
+                PRECISAM DA <strong>SUA AJUDA!</strong>
+              </span>
 
               <ButtonContainer>Quero ajudar!</ButtonContainer>
             </div>
 
-            <img src="/imgs/together.svg" alt="Juntos" />
+            <img src="/imgs/main.svg" alt="Background" />
           </Content>
         </Wrapper>
 
         <Section id="about">
-          <h1>O que é o Faz um bem?</h1>
-          <ContentHeading>
-            <p>
-              “Faz um bem!” surge em um contexto de emergência de saúde pública
-              mundial com a pandemia de Coronavírus Disease 2019 (COVID-19) que
-              intensifica a necessidade de ações sociais por parte da sociedade
-              civil organizada e do Estado, especialmente através das políticas
-              públicas e sociais. Trata-se de um projeto de caráter
-              extensionista interdisciplinar, que visa desenvolver
-              colaborativamente e impulsionar uma plataforma virtual interativa
-              que centralize necessidades de instituições e organizações sociais
-              de Santa Maria. Assim, Faz um bem! propõe-se como uma plataforma
-              incentivadora e mediadora entre doadores e receptores de recursos.
-            </p>
+          <div>
+            <img src="/imgs/about.svg" alt="Sobre" />
 
-            <img src="/imgs/hugging.svg" alt="Abraçando" />
-          </ContentHeading>
+            <div />
 
-          <ContentFullText>
+            <div>
+              <span>
+                O que é o <strong>Faz um bem?</strong>
+              </span>
+
+              <p>
+                “Faz um bem!” surge em um contexto de emergência de saúde
+                pública mundial com a pandemia de Coronavírus Disease 2019
+                (COVID-19) que intensifica a necessidade de ações sociais por
+                parte da sociedade civil organizada e do Estado, especialmente
+                através das políticas públicas e sociais. Trata-se de um projeto
+                de caráter extensionista interdisciplinar, que visa desenvolver
+                colaborativamente e impulsionar uma plataforma virtual
+                interativa que centralize necessidades de instituições e
+                organizações sociais de Santa Maria. Assim, Faz um bem!
+                propõe-se como uma plataforma incentivadora e mediadora entre
+                doadores e receptores de recursos.
+              </p>
+            </div>
+          </div>
+
+          <p>
             A plataforma se justifica pela necessidade constante das
             instituições e entidades de assistência social divulgarem suas ações
             e arrecadarem recursos com vistas à continuidade da prestação de
@@ -72,77 +118,111 @@ export default function Home() {
             perspectiva do acesso e usufruto de seus direitos sociais. Essas
             necessidades são aumentadas no contexto da atual pandemia, que tem
             tido consequência para as esferas social, econômica e política.
-          </ContentFullText>
+          </p>
 
-          <ContentFullText>
+          <p>
             A plataforma conta com uma ampla equipe Coordenada por professores
             de diversos Centros da Universidade Federal de Santa Maria. Essa
             parceria permite a inclusão de estudantes de áreas de conhecimentos
             distintas, possibilitando o enriquecimento da formação acadêmica dos
             estudantes na perspectiva da interdisciplinaridade e do trabalho em
             equipe.
-          </ContentFullText>
-
-          <ContentImageLeft>
-            <span />
-            <p>
-              A plataforma cadastra campanhas e instituições que possam fazer a
-              diferença na vida de famílias e comunidas. Você busca campanhas ou
-              instituições e vê como pode ajudar. As ajudas funcionam
-              diretamente com os promotores das campanhas, os quais abastecem a
-              plataforma com informações. É responsabilidade da plataforma
-              conectar doadores e campanhas, tornando mais fácil a visualização
-              e o acesso.
-            </p>
-          </ContentImageLeft>
-
-          <ContentImageLeft>
-            <span />
-            <p>
-              As intituições são responsáveis pelo conteúdo disponibilizado,
-              assim como pela execução das doações. A plataforma não é
-              responsável pela captação de recursos, nem pela entrega às
-              instituições.
-            </p>
-          </ContentImageLeft>
-
-          <ContentImageLeft>
-            <span />
-            <p>
-              As instituições são responsáveis pelo conteúdo disponibilizado,
-              assim como pela execução das doações. A plataforma não é
-              responsável pela captação de recursos, nem pela entrega às
-              instituições.
-            </p>
-          </ContentImageLeft>
-
-          <ContentImageLeft>
-            <span />
-            <p>
-              Para ser um apoiador, ajude a divulgar essa plataforma, para que
-              chegue a um número enorme de pessoas que possam fazer a diferença.
-              Entre em contato pelo email: fazumbem@inf.ufsm.br e saiba mais.
-            </p>
-          </ContentImageLeft>
-
-          <ContentImageLeft>
-            <span />
-
-            <FormContainer id="contact">
-              <InputContent placeholder="Nome*" />
-              <InputContent placeholder="Telefone*" />
-              <InputContent placeholder="E-mail*" />
-
-              <TextareaContent placeholder="Mensagem*" />
-
-              <div>
-                <span>(Todos os campos* são obrigatórios)</span>
-                <SubmitButton type="submit">Enviar</SubmitButton>
-              </div>
-            </FormContainer>
-          </ContentImageLeft>
+          </p>
         </Section>
 
+        <MoreInfo>
+          <MoreInfoContent>
+            <div>
+              <h1>Como funciona?</h1>
+
+              <img src="/imgs/building.svg" alt="Como funciona?" />
+
+              <p>
+                A plataforma cadastra campanhas e instituições que possam{' '}
+                <strong>fazer o bem</strong> o bem. Você busca campanhas ou
+                instituições e vê como pode ajudar. As ajudas funcionam
+                diretamente com os promotores das campanhas, os quais abastecem
+                a plataforma com informações.
+              </p>
+            </div>
+
+            <div>
+              <h1>Termos de uso</h1>
+
+              <img src="/imgs/terms.svg" alt="Termos de uso" />
+
+              <p>
+                As intituições são responsáveis pelo conteúdo disponibilizado,
+                assim como pela execução das doações.{' '}
+                <strong>
+                  A plataforma não é responsável pela captação de recursos, nem
+                  pela entrega às instituições.
+                </strong>
+              </p>
+            </div>
+
+            <div>
+              <h1>Como apoiar?</h1>
+
+              <img src="/imgs/map.svg" alt="Como apoiar?" />
+
+              <p>
+                Para ser um apoiador, ajude a divulgar essa plataforma, para que
+                chegue a um número enorme de pessoas que possam fazer a
+                diferença. Entre em contato pelo email:{' '}
+                <strong>fazumbem@inf.ufsm.br</strong> e saiba mais.
+              </p>
+            </div>
+          </MoreInfoContent>
+        </MoreInfo>
+
+        <ContactContainer id="contact">
+          <div className="background_gray" />
+          <div className="background_purple" />
+          <div className="content">
+            <img src="/imgs/contact.svg" alt="Contato" />
+
+            <div>
+              <FormContainer onSubmit={handleSubmit(handleSubmitForm)}>
+                <InputContent
+                  name="name"
+                  icon={FiUser}
+                  placeholder="Nome*"
+                  error={errors.name}
+                  {...register('name')}
+                />
+                <InputContent
+                  name="phone"
+                  icon={FiPhone}
+                  placeholder="Telefone*"
+                  error={errors.phone}
+                  {...register('phone')}
+                />
+                <InputContent
+                  name="email"
+                  icon={FiMail}
+                  placeholder="E-mail*"
+                  error={errors.email}
+                  {...register('email')}
+                />
+
+                <TextareaContent
+                  name="message"
+                  placeholder="Mensagem*"
+                  error={errors.message}
+                  {...register('message')}
+                />
+
+                <div>
+                  <span>(Todos os campos* são obrigatórios)</span>
+                  <SubmitButton type="submit">
+                    {formState.isSubmitting ? 'Enviando...' : 'Enviar'}
+                  </SubmitButton>
+                </div>
+              </FormContainer>
+            </div>
+          </div>
+        </ContactContainer>
         <Footer />
       </Container>
     </>
--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -11,44 +11,66 @@ export const Container = styled.div`
 `;
 
 export const Wrapper = styled.div`
-  min-height: calc(100vh - 8rem);
+  height: 50rem;
   width: 100%;
-  background: linear-gradient(120deg, #034074, rgba(3, 64, 115, 0.6));
+  background: var(--purple);
 
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  padding: 2.5rem 0;
+  position: relative;
+`;
+
+export const BackgroundContent = styled.div`
+  height: 50rem;
+  width: 70%;
+  background: var(--yellow);
+  position: absolute;
+  left: 0;
 `;
 
 export const Content = styled.main`
   max-width: 1120px;
   width: 100%;
+  height: 100%;
+
+  position: relative;
+  z-index: 2;
 
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 2fr 2fr;
 
   margin: auto;
+
+  img {
+    height: 50rem;
+  }
 
   div {
     margin: auto 0;
 
-    h1 {
-      color: var(--white);
-      font-size: 4rem;
-      line-height: 4rem;
-      width: 440px;
+    span {
+      width: 424px;
+      color: var(--blue);
+      font-size: 30px;
+      line-height: 3rem;
       font-weight: normal;
+      text-align: justify;
     }
   }
 `;
 
 export const ButtonContainer = styled(Button)`
-  background: var(--yellow);
-  margin-top: 8rem;
-  height: 6rem;
-  width: 40rem;
+  background: var(--orange);
+  margin-top: 6rem;
+  border-radius: 1.5rem;
+  font-size: 2rem;
+
+  :hover {
+    border-color: var(--orange);
+    color: var(--orange);
+  }
 `;
 
 export const Section = styled.section`
@@ -58,7 +80,41 @@ export const Section = styled.section`
   max-width: 1120px;
   width: 100%;
 
-  margin: 3rem 0;
+  margin-top: 5rem;
+  padding: 10rem 0 5rem;
+
+  position: relative;
+
+  img {
+    position: absolute;
+    width: 40rem;
+    top: 0;
+    left: 2rem;
+  }
+
+  p {
+    font-size: 1.6rem;
+    line-height: 3rem;
+    margin-top: 2rem;
+    text-align: justify;
+  }
+
+  > div {
+    display: grid;
+    grid-template-columns: 2fr 7.5fr;
+
+    div {
+      display: flex;
+      flex-direction: column;
+
+      span {
+        color: var(--blue);
+        font-size: 3rem;
+        text-align: right;
+        margin-bottom: 2.5rem;
+      }
+    }
+  }
 
   h1 {
     font-size: 3rem;
@@ -66,75 +122,134 @@ export const Section = styled.section`
   }
 `;
 
-export const ContentHeading = styled.div`
+export const MoreInfo = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+
+  background: var(--yellow);
+
+  padding: 7.5rem 0;
+`;
+
+export const MoreInfoContent = styled.div`
+  width: 100%;
+  max-width: 1120px;
+
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: repeat(3, 1fr);
+  grid-column-gap: 5rem;
 
-  margin-top: 1rem;
+  > div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-  p {
+    padding: 2rem 3rem;
+
+    background: var(--white);
+    box-shadow: 0 2px 4px 1px var(--gray);
+
     font-size: 1.5rem;
-    text-align: justify;
-  }
+    color: var(--heading);
 
-  img {
-    margin: 0 auto;
-    width: 28rem;
+    h1 {
+      font-size: 2rem;
+      text-transform: uppercase;
+    }
+
+    img {
+      margin-top: 2rem;
+    }
+
+    p {
+      margin-top: 2rem;
+      line-height: 3rem;
+      text-align: center;
+    }
   }
 `;
 
-export const ContentFullText = styled.p`
-  font-size: 1.5rem;
-  margin-top: 3rem;
-  text-align: justify;
-`;
+export const ContactContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex: 1;
+  width: 100%;
 
-export const ContentImageLeft = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 2fr;
+  position: relative;
 
-  margin-top: 3rem;
+  > div {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    position: relative;
+    width: 100%;
 
-  p {
-    font-size: 1.5rem;
-    text-align: justify;
-  }
-`;
+    &.background_gray {
+      display: flex;
+      background: var(--backgorund-gray);
+      width: 50%;
+      height: 100%;
+      position: absolute;
+      z-index: 0;
+      top: 0;
+      left: 0;
+    }
 
-export const ContentImageRight = styled.div`
-  display: grid;
-  grid-template-columns: 2fr 1fr;
+    &.background_purple {
+      display: flex;
+      background: var(--background-purple);
+      width: 50%;
+      min-height: 100%;
+      position: absolute;
+      z-index: 0;
+      top: 0;
+      right: 0;
+    }
 
-  margin-top: 3rem;
+    &.content {
+      max-width: 1120px;
+    }
 
-  p {
-    font-size: 1.5rem;
-    text-align: justify;
+    img {
+      width: 500px;
+      margin: 5rem 0;
+      margin-right: -100px;
+      position: relative;
+    }
+
+    > div {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      justify-content: center;
+
+      background: var(--purple);
+    }
   }
 `;
 
 export const FormContainer = styled.form`
-  max-width: 350px;
-  margin: 3rem auto 0;
+  width: 100%;
+  padding-left: 225px;
 
-  div {
+  > div:last-child {
     display: grid;
-    grid-template-columns: 1.5fr 1fr;
+    grid-template-columns: 3fr 2fr;
+    grid-gap: 1rem;
+
+    margin-top: 1.5rem;
 
     span {
-      font-size: 1rem;
-      color: var(--description);
+      color: var(--text-white);
+      font-size: 1.3rem;
     }
   }
 `;
 
 export const InputContent = styled(Input)``;
 
-export const TextareaContent = styled(Textarea)`
-  margin: 1rem 0;
-`;
+export const TextareaContent = styled(Textarea)``;
 
-export const SubmitButton = styled(Button)`
-  height: 4rem;
-  font-size: 1.5rem;
-`;
+export const SubmitButton = styled(Button)``;
