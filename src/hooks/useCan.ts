19dfae4573344b3a2801bca1138dc83554d5fb9f
wedersonf <wedymf@hotmail.com>
2021-07-18 19:41:19 -0300

    Criando hook de autorização

---
+++ src/hooks/useCan.ts
@@ -0,0 +1,33 @@
+import { useContext } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
+
+type UseCanProps = {
+  role?: number;
+  permission?: number;
+};
+
+export function useCan({ role, permission }: UseCanProps) {
+  const { user, isAuthenticated } = useContext(AuthContext);
+
+  if (!isAuthenticated) {
+    return false;
+  }
+
+  if (role) {
+    const hasRole = user?.role !== Number(role);
+
+    if (!hasRole) {
+      return false;
+    }
+  }
+
+  if (permission) {
+    const hasPermission = user?.permission === Number(permission);
+
+    if (!hasPermission) {
+      return false;
+    }
+  }
+
+  return true;
+}
