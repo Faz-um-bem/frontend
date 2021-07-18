4acc2a5dff69719741c9ff069f3c3700f460dbe5
wedersonf <wedymf@hotmail.com>
2021-07-18 19:42:08 -0300

    Funções de autenticação SSR

---
+++ src/utils/withSSRAuth.ts
@@ -0,0 +1,25 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from 'next';
+import { parseCookies } from 'nookies';
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
+  return async (
+    ctx: GetServerSidePropsContext,
+  ): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);
+
+    if (!cookies['fazumbem.token']) {
+      return {
+        redirect: {
+          destination: '/sign',
+          permanent: false,
+        },
+      };
+    }
+
+    return await fn(ctx);
+  };
+}
