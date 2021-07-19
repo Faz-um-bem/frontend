e680e50b6a49b9ffb4a2b40af9adef3c8973e01e
wedersonf <wedymf@hotmail.com>
2021-07-19 02:11:48 -0300

    Adicionando acessibilidade do modal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import React, { ReactNode } from 'react';
 import { AppProps } from 'next/app';
+import Modal from 'react-modal';
 
 import GlobalStyles from '../styles/global';
 
 import { AuthProvider } from '../contexts/AuthContext';
+
+Modal.setAppElement('#__next');
 
 function MyApp({ Component, pageProps }: AppProps): ReactNode {
   return (
