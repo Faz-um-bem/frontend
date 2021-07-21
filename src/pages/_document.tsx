1da39b8596cc5bfdb43427c1b86900cb35166eac
wedersonf <wedymf@hotmail.com>
2021-07-21 00:40:06 -0300

    Correção crossorigin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,7 +40,11 @@ export default class MyDocument extends Document {
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400&display=swap"
             rel="stylesheet"
